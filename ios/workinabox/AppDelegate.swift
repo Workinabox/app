ebac2473d4c425a481e39ce4935ec9ec5c7ce162
Frederic Gos <frederic.gos@gmail.com>
2026-03-11 09:26:34 +0000

    fix: replace remaining huddletalk_app references in ios files

--- ios/workinabox/AppDelegate.swift
+++ ios/workinabox/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "huddletalk_app",
+      withModuleName: "Workinabox",
       in: window,
       launchOptions: launchOptions
     )
